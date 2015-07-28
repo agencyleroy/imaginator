1b4753b3125e896e7beac998401fbbe34dd63fa1
Joakim Runeberg <joakim.runeberg@gmail.com>
2015-07-28 13:37:20 +0300

    better image fetching

--- web/controllers/image_controller.ex
+++ web/controllers/image_controller.ex
@@ -37,7 +37,8 @@ defmodule Imaginator.ImageController do
   end
 
   def render_image(params) do
-    image = open("./priv/static/images/lundia.jpg")
+    number = 1
+    image = open("./priv/static/images/#{number}.jpg")
     image2 = image
 
     image
